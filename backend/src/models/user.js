fada006aff48d2ef78201b31cec8b5a73c496ad2
powder <powder@mail.com>
2024-03-02 21:28:22 +0700

    bcrypt password before adding to database

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -7,7 +8,19 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true },
 });
 
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password") && this.password) {
+    try {
+      const saltRounds = 10;
+      const salt = await bcrypt.genSalt(saltRounds);
+      this.password = await bcrypt.hash(this.password, salt);
+    } catch (err) {
+      console.log(err);
+      return next(err);
+    }
+  }
+  next();
+});
+
 const User = mongoose.model("User", userSchema);
-
 export default User;
-
