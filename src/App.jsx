9574369859ab8773aad9b2f347840c6801f00051
Huỳnh Ngọc Xuân <xuanhuynh.170204@gmail.com>
2024-03-22 14:26:42 +0700

    header

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import {
+  Routes,
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
