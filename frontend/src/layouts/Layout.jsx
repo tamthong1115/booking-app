5a3bbf25a526d365372561213b1a7a4f31f7d039
Huỳnh Ngọc Xuân <xuanhuynh.170204@gmail.com>
2024-03-22 19:40:46 +0700

    header

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,10 +1,16 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import classNames from "classnames/bind";
+import styles from './Layout.module.scss';
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+
+const cx = classNames.bind(styles)
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
+    <div >
+      <div className={cx('container')}>
+        <Header />
+      </div>
       <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer />
     </div>
